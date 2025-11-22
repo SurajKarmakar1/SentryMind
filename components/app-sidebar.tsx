366089933152bde2fa04539e6046f700641b2b78
surajtheneko07 <nekodev1807@gmail.com>
2025-11-22 21:58:10 +0530

    update

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -37,6 +37,7 @@ const data = {
     {
       name: "Sentry Mind",
       logo: GalleryVerticalEnd,
+      plan: "Enterprise",
     },
   ],
   navMain: [
@@ -148,7 +149,9 @@ const data = {
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader></SidebarHeader>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
         <NavProjects projects={data.projects} />
